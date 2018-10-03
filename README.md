fd4b35276a02f1d9c44ef0c643f6952529eda4bd
Jeremy Meguira <j.meguira19@gmail.com>
2018-10-03 14:52:04 -0400

    added first level of word tunneling + README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Challenge 366
+
+Challenge from https://www.reddit.com/r/dailyprogrammer/comments/99d24u/20180822_challenge_366_intermediate_word_funnel_2
+
+challenge_366.py - source for challenge solution
+README.md - this file
+
+run instructions:
+python challenge_366.py <word>
+
+where word is the word that you would like to tunnel
--- challenge_366.py
+++ challenge_366.py
@@ -2,18 +2,38 @@ import requests
 import sys
 import hashlib
 
+url = 'https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt'
+r = requests.get(url)
+content = r.text
+ht = {}
+for x in content.split('\n'):
+    ht[hashlib.blake2b(x.encode('utf-8')).digest()] = x
+
+def get_words(word):
+
+    out = []
+    for i in range(len(word)-1):
+        try:
+            temp = word[:i] + word[i+1:]
+            #https://stackoverflow.com/a/3559592
+            temphash = hashlib.blake2b(temp).digest()
+            out.append(ht[temphash])
+        except KeyError:
+            continue
+    return out
+
 def wfunnel(word):
-    url = 'https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt'
-    r = requests.get(url)
-    content = r.text
-    ht = {}
-    for x in content.split('\n'):
-        ht[hashlib.blake2b(x.encode('utf-8')).digest()] = x
 
     temphash = hashlib.blake2b(word).digest()
-    if ht[temphash]:
-        print('yes is word')
+    try:
+        print(str(ht[temphash]) + ' is a valid starting word')
+    except KeyError:
+        print(sys.argv[1] +  ' is not a valid starting word')
+        return
 
+    quit = False
+    test = get_words(word)
+    print(len(test))
 
 def main():
     print('script : ' + sys.argv[0] )
